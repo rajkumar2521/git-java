298d1c2233a32e199c98d06f3afc887825052e3d
rajkumar2521 <56027375+rajkumar2521@users.noreply.github.com>
2021-07-31 10:50:33 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,6 +2,7 @@ class Sample {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Hello Maven!"); 
+         System.out.println("Hello Jenkins"); 
         
     }
 }
